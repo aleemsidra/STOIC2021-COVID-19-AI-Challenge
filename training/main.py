cd8921e8dadd3ec04a10eba8a28cfd57d5e37d12
sidra <sidra.aleem2@mail.dcu.ie>
2022-05-13 12:52:46 +0100

    copied working main, train and preprocess from docker

--- training/main.py
+++ training/main.py
@@ -1,17 +1,58 @@
+
 import shutil
 from train import do_learning
+import os
+from preprocess import preprocess_dir
+import pandas as pd
 
 DATA_DIR = "/input/"
 ARTIFACT_DIR = "/output/"
 
+SCRATCH_DIR = "/scratch/"
+
 if __name__ == "__main__":
     # Substitute do_learning for your training function.
     # It is recommended to write artifacts (e.g. model weights) to ARTIFACT_DIR during training.
-    artifacts = do_learning(DATA_DIR, ARTIFACT_DIR)
 
-    # When the learning has completed, any artifacts should have been saved to ARTIFACT_DIR.
-    # Alternatively, you can copy artifacts to ARTIFACT_DIR after the learning has completed:
-    for artifact in artifacts:
-        shutil.copy(artifact, ARTIFACT_DIR)
 
-    print("Training completed.")
+    suffixes= ['1','2','3','4','5']
+    # artifacts = do_learning(DATA_DIR, ARTIFACT_DIR)
+
+
+    preprocess_dir(DATA_DIR, SCRATCH_DIR)
+    shutil.copy(os.path.join(DATA_DIR, 'reference.csv'), SCRATCH_DIR)
+
+    # do splits
+
+    ref = pd.read_csv(os.path.join(SCRATCH_DIR, 'reference.csv'))
+
+    for suffix in suffixes:
+
+        ref_sampled = ref.sample(frac=1)
+        n = len(ref_sampled)
+        train_n = int(0.8*n)
+
+        train_df = ref_sampled.iloc[:train_n]
+        val_df = ref_sampled.iloc[train_n:]
+
+        print('First patient of train df ', train_df.iloc[0])
+
+        train_df.to_csv(os.path.join(SCRATCH_DIR, f'train_{suffix}.csv'),index=False)
+
+        train_df.to_csv(os.path.join(SCRATCH_DIR, f'val_{suffix}.csv'),index=False)
+
+        config = f'./configs/config_mobile_{suffix}.json'
+
+
+        artifacts = do_learning(config, suffix)
+
+        print('artifacts ', artifacts)
+        shutil.copy(artifacts[0], os.path.join(ARTIFACT_DIR, suffix+'.pth.tar'))
+
+        # When the learning has completed, any artifacts should have been saved to ARTIFACT_DIR.
+        # Alternatively, you can copy artifacts to ARTIFACT_DIR after the learning has completed:
+
+
+        print("Training completed for suffix ", suffix)
+
+    print("Training completed")
--- training/preprocess.py
+++ training/preprocess.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import numpy as np
 import SimpleITK as sitk
 import os
@@ -6,17 +6,21 @@ import cv2
 import pandas as pd
 import random
 import shutil
-from PIL import Image 
+from PIL import Image
 from tqdm import tqdm
 
+from multiprocessing import Pool
+import functools
+
 def sub_samples(path, i):
-    
+
+    # print('path + i ', path + i)
     input_image = sitk.ReadImage(path + i)
 #     print('input_image.shape ', input_image.GetSize())
 
     slice = int(np.floor(input_image.GetSize()[2]/33))
     sub_sample = input_image[:, :, 0:input_image.GetDepth():slice]
-    sub_sample = sitk.GetArrayFromImage(sub_sample) 
+    sub_sample = sitk.GetArrayFromImage(sub_sample)
     sub_sample = sub_sample[:32, :, :]
 #     print('subsample.shape', sub_sample.shape)
 
@@ -25,34 +29,43 @@ def sub_samples(path, i):
 
 # windowing
 def window(img, w, l):
-    
+
     x = l + w/2
     y = l - w/2
     img[img > x] = x
     img[img < y] = y
     chanel = (img - np.min(img))/(np.max(img) - np.min(img))
-    
+
     return chanel
 
+def preprocess_single(i, args):
+    path = args[0]
+    root_dir = args[1]
+    sub_path = root_dir
+    # print('i', i)
+    # print('path ', root_dir)
+    # print('root_dir ', path)
+    if not os.path.exists(os.path.join(sub_path , i.split('.')[0])):
+        os.mkdir(os.path.join(sub_path , i.split('.')[0]))
+
+    sub_sample = sub_samples(path, i)
+
+    image = window(sub_sample, 1500, -600)
+    for j in range(image.shape[0]):
+        cv2.imwrite(os.path.join(sub_path , i.split('.')[0], str(j) + '.png'), image[j,:,:]*255)
 
 
-def process_dir():
+def preprocess_dir(mha_dir, scratch_dir):
 
 
-path = "/home/sidra/Documents/STOIC/stoic_2022_final_phase/data/mha/"
-root_dir = "/home/sidra/Documents/STOIC/stoic_2022_final_phase/"
+    img_list = os.listdir(mha_dir)
+    # sub_path = os.path.join(root_dir, "scratch")
 
-img_list = os.listdir(path)  
-sub_path = os.path.join(root_dir, "scratch")
 
-j=0
-for i in tqdm(img_list):
- if not os.path.exists(os.path.join(sub_path , i.split('.')[0])):
-    os.mkdir(os.path.join(sub_path , i.split('.')[0]))
+    img_list = [i for i in img_list if '.mha' in i][:40]
 
- sub_sample = sub_samples(path, i)   
- j=j+1
+    print('total images to be preprocessed ', len(img_list))
+    # asd
+    with Pool(16) as p:
+        r = list(tqdm(p.map(functools.partial(preprocess_single, args=(mha_dir, scratch_dir)), img_list), total=len(img_list)) )
 
- image = window(sub_sample, 1500, -600)
- for j in range(image.shape[0]):
-    cv2.imwrite(os.path.join(sub_path , i.split('.')[0], str(j) + '.png'), image[j,:,:]*255)
